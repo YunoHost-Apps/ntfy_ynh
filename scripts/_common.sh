3f3cbc232cfd7b504bee3ba190543e29c341e644
Alexander Wühr <awuehr@pnyhf.eu>
2023-02-24 15:51:41 +0100

    Fix mooaaaar

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,13 +9,12 @@
 #=================================================
 ntfy_setup_source() {
     ynh_setup_source -d "$install_dir" -s "$YNH_ARCH"
+    mkdir -p "$install_dir/data"
     chown -R $app:$app "$install_dir"
+    chmod -R 750 "$install_dir/data"
 
     ynh_secure_remove "$install_dir/client"
     ynh_secure_remove "$install_dir/server"
-
-    mkdir -p "$install_dir/data"
-    chmod -R 750 "$install_dir/data"
 }
 
 #=================================================
