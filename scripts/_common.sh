f69599207ee3baac235eca1f3e695259039a8060
Alexander Wühr <awuehr@pnyhf.eu>
2023-02-24 14:26:31 +0100

    omg kill me

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,8 +8,8 @@
 # PERSONAL HELPERS
 #=================================================
 ntfy_setup_source {
-    $YNH_ARCH=$1
-    $install_dir=$2
+    # $YNH_ARCH=$1
+    # $install_dir=$2
 
     ynh_setup_source -d "$install_dir" -a "$YNH_ARCH"
     ynh_secure_remove --file=$install_dir/client
@@ -19,9 +19,9 @@ ntfy_setup_source {
 }
 
 ntfy_fix_permissions {
-    $app=$1
-    $install_dir=$2
-    
+    # $app=$1
+    # $install_dir=$2
+
     chmod o+w "$install_dir/data"
     chmod 400 "$install_dir/server.yml"
     chown $app:$app "$install_dir/server.yml"
