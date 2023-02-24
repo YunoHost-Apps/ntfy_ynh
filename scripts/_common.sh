d31645f435ff00b43d0292b205625ec28f4ff3ee
Alexander Wühr <awuehr@pnyhf.eu>
2023-02-24 14:23:37 +0100

    (Hopefully) fix some more errors

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,10 @@
 #=================================================
 # PERSONAL HELPERS
 #=================================================
-ntfy_setup_source() {
+ntfy_setup_source {
+    $YNH_ARCH=$1
+    $install_dir=$2
+
     ynh_setup_source -d "$install_dir" -a "$YNH_ARCH"
     ynh_secure_remove --file=$install_dir/client
     ynh_secure_remove --file=$install_dir/server
@@ -15,7 +18,10 @@ ntfy_setup_source() {
     mkdir -p "$install_dir/data"
 }
 
-ntfy_fix_permissions() {
+ntfy_fix_permissions {
+    $app=$1
+    $install_dir=$2
+    
     chmod o+w "$install_dir/data"
     chmod 400 "$install_dir/server.yml"
     chown $app:$app "$install_dir/server.yml"
