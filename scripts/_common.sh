68d4dc238b4c4e878e02cd3690c45451d0c67610
Alexander Wühr <awuehr@pnyhf.eu>
2023-02-24 14:01:24 +0100

    Switch to packaging version v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,6 +7,19 @@
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+ntfy_setup_source {
+    ynh_setup_source -d "$install_dir" -a "$YNH_ARCH"
+    ynh_secure_remove --file=$install_dir/client
+    ynh_secure_remove --file=$install_dir/server
+
+    mkdir -p "$install_dir/data"
+}
+
+ntfy_fix_permissions {
+    chmod 400 "$install_dir/server.yml"
+    chown $app:$app "$install_dir/server.yml"
+    chmod +x "$install_dir/ntfy.sh"
+}
 
 #=================================================
 # EXPERIMENTAL HELPERS
